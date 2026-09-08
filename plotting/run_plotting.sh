9e20302e6db817eb4fd7a24f035d6d116b186950
Lindsey Riggs <lmr294@ogmios.ccib.rutgers.edu>
2026-09-08 13:18:04 -0400

    reran a function

--- plotting/run_plotting.sh
+++ plotting/run_plotting.sh
@@ -33,11 +33,11 @@ mkdir -p "$FIGURE_DIR"
 # python3 plot_rg_vs_termini_correlation.py --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR" 		
 
 # # Plot the frequencies of contact states
-# python3 plot_state_freq.py --association 8_2_to_2_16   --bar_plot_color darkblue --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
-# python3 plot_state_freq.py --association 8_16_to_2_16  --bar_plot_color maroon --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
-# python3 plot_state_freq.py --association 8_12_to_12_2  --bar_plot_color darkgreen --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
-# python3 plot_state_freq.py --association 12_2_to_2_16  --bar_plot_color indigo --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
-# python3 plot_state_freq.py --association 8_12_to_12_16 --bar_plot_color palevioletred --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
+python3 plot_state_freq.py --association 8_2_to_2_16   --bar_plot_color darkblue --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
+python3 plot_state_freq.py --association 8_16_to_2_16  --bar_plot_color maroon --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
+python3 plot_state_freq.py --association 8_12_to_12_2  --bar_plot_color darkgreen --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
+python3 plot_state_freq.py --association 12_2_to_2_16  --bar_plot_color indigo --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
+python3 plot_state_freq.py --association 8_12_to_12_16 --bar_plot_color palevioletred --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
 
 # # # Plot odds ratios
 # python3 plot_odds_ratios.py --association 8_2_to_2_16   --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
@@ -51,4 +51,4 @@ mkdir -p "$FIGURE_DIR"
 # python3 plot_one_fig_range_residue_contacts.py --state MN_NC --x-range 105 111 --y-range 31 38 --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR"
 
 # # Plot blob-blob contacts  
-python3 plot_blob_contacts_one_fig.py --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR" 			
+# python3 plot_blob_contacts_one_fig.py --input_path "$INPUT_DIR" --output_path "$FIGURE_DIR" 			
