13d0fc7d39bcc0279b705967181d9d9b1553c100
Lindsey Riggs <lmr294@ogmios.ccib.rutgers.edu>
2026-09-08 14:19:51 -0400

    made sequences a variable

--- post_processing/process_trajectory.sh
+++ post_processing/process_trajectory.sh
@@ -10,14 +10,25 @@ TRAJ_DIR="$ROOT_DIR/trajectories/raw"
 PROCESSED_TRAJ_DIR="$ROOT_DIR/trajectories/post_processed11"
 DATA_DIR="$ROOT_DIR/data11"
 
+SEQUENCES="${SEQUENCES:-F66 M66 V66 L66 A66 Y66 I66}"
+EQUILIBRATION_TIME=8000
+
 mkdir -p "$PROCESSED_TRAJ_DIR"
 mkdir -p "$DATA_DIR"
 
-python3 manipulate_pbc.py --raw_traj_path "$TRAJ_DIR" --processed_traj_path "$PROCESSED_TRAJ_DIR"
+# Make protein whole, wrap, and center
+python3 manipulate_pbc.py \
+  --raw_traj_path "$TRAJ_DIR" \
+  --processed_traj_path "$PROCESSED_TRAJ_DIR"
 
-python3 calc_mindist.py --raw_traj_path "$TRAJ_DIR" --input_traj_path "$PROCESSED_TRAJ_DIR" --output_data_path "$DATA_DIR"
+# Calculate the minimum distance between the protein and its periodic image. Write frame numbers to a list where distance is < cutoff
+python3 calc_mindist.py \
+  --raw_traj_path "$TRAJ_DIR" \
+  --input_traj_path "$PROCESSED_TRAJ_DIR" \
+  --output_data_path "$DATA_DIR"
 
-for seq in F66 M66 V66 L66 A66 Y66 I66; do
+# Remove periodic image frames from trajectory
+for seq in $SEQUENCES; do
   vmd -dispdev none \
       -e run_PIF_script.tcl \
       -eofexit \
@@ -30,11 +41,13 @@ for seq in F66 M66 V66 L66 A66 Y66 I66; do
     gmx trjconv -f ${PROCESSED_TRAJ_DIR}/${seq}_wrapped_centered_PIF_del.gro -o ${PROCESSED_TRAJ_DIR}/${seq}_wrapped_centered_PIF_del.xtc
 done
 
-for seq in F66 M66 V66 L66 A66 Y66 I66; do
+# Remove equilibration time 
+for seq in $SEQUENCES; do
   vmd -dispdev none \
       -e run_equil_script.tcl \
       -eofexit \
       -args \
+      "$EQUILIBRATION_TIME"
       "$TRAJ_DIR/${seq}/${seq}.gro" \
       "$PROCESSED_TRAJ_DIR/${seq}_wrapped_centered_PIF_del.xtc" \
       "$PROCESSED_TRAJ_DIR" \
--- post_processing/run_equil_script.tcl
+++ post_processing/run_equil_script.tcl
@@ -1,8 +1,8 @@
 # Set arguments
-set equil_time 8000
-set gro_name [lindex $argv 0]
-set xtc_name [lindex $argv 1]
-set processed_dir [lindex $argv 2]
+set equil_time [lindex $argv 0]
+set gro_name [lindex $argv 1]
+set xtc_name [lindex $argv 2]
+set processed_dir [lindex $argv 3]
 
 # Load sequence into VMD and remove the first frame since it is the gro structure
 mol new $gro_name type gro waitfor all
