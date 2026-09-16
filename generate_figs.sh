5e7d62a3f2e3cb205f480814968f9ae17bf8bfc9
Lindsey Riggs <lindseymriggs@yahoo.com>
2026-09-16 16:22:16 -0400

    added command to download zenodo repo directly

--- generate_figs.sh
+++ generate_figs.sh
@@ -3,6 +3,12 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Download data from zenodo
+zenodo_get 10.5281/zenodo.22710682
+
+# Unzip data
+unzip *.zip
+
 echo "Processing trajectories..."
 bash "$SCRIPT_DIR/post_processing/process_trajectory.sh"
 
